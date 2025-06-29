88291f8b250e91b5119e43d24f9d7f5ee89a3ec3
algof <algofkz@gmail.com>
2025-06-29 21:52:12 +0700

    revisi server.py

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ import sys
 import logging
 from httpfile import HttpServer
 
-# Global instance of the HTTP server logic
+# Global instance dari HTTP server
 httpserver = HttpServer()
 
 class ProcessTheClient(threading.Thread):
@@ -16,54 +16,27 @@ class ProcessTheClient(threading.Thread):
         threading.Thread.__init__(self)
 
     def run(self):
-        # A more robust way to receive HTTP requests
+        
         raw_request = bytearray()
-        # First, read headers
+        
         while True:
             try:
                 d = self.connection.recv(1024)
                 if not d:
                     break
                 raw_request += d
-                # A simple check for end of headers. For POST, we might need more.
+                # Pengecekan end of header
                 if b'\r\n\r\n' in raw_request:
                     break
             except OSError:
                 break
 
-        # Decode and process if we received anything
-        if not raw_request:
+        # Jika tidak ada data yang dikirimkan, close koneksi
+        if not raw_request: 
             self.connection.close()
             return
             
-        request_string = raw_request.decode('utf-8', errors='ignore')
-
-        # Check for Content-Length to read the full body correctly for POST requests
-        headers = request_string.split('\r\n')
-        content_length = 0
-        for h in headers:
-            if h.lower().startswith('content-length:'):
-                try:
-                    content_length = int(h.split(':')[1].strip())
-                    break
-                except (ValueError, IndexError):
-                    pass
-        
-        # Ensure the full body has been received
-        header_end = request_string.find('\r\n\r\n')
-        if header_end != -1:
-            body_part_bytes = raw_request[header_end+4:]
-            while len(body_part_bytes) < content_length:
-                 try:
-                    d = self.connection.recv(1024)
-                    if not d:
-                        break
-                    raw_request += d
-                    body_part_bytes += d
-                 except OSError:
-                    break
-        
-        # Process the full request using the imported httpserver instance
+        # Proses full request dengan httpserver instance
         full_request_string = raw_request.decode('utf-8', errors='ignore')
         try:
             logging.warning(f"data dari client: {self.address} -> {full_request_string.splitlines()[0]}")
@@ -72,7 +45,10 @@ class ProcessTheClient(threading.Thread):
 
         hasil = httpserver.proses(full_request_string)
         
+        # Kirim hasil ke client yang terhubung
         self.connection.sendall(hasil)
+
+        # Tutup koneksi
         self.connection.close()
 
 class Server(threading.Thread):
